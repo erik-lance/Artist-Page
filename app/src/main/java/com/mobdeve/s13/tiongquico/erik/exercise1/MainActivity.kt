8583a9ab5b26e27be6cb7885df2fccc63f51899b
erik-lance <eriklance@gmail.com>
2023-09-23 00:07:04 +0800

    Added purchase button functionality

--- app/src/main/java/com/mobdeve/s13/tiongquico/erik/exercise1/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s13/tiongquico/erik/exercise1/MainActivity.kt
@@ -2,6 +2,7 @@ package com.mobdeve.s13.tiongquico.erik.exercise1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.core.content.ContextCompat
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
@@ -10,12 +11,15 @@ class MainActivity : AppCompatActivity() {
     private lateinit var favorite: FloatingActionButton
     private var isFavorite: Boolean = false
 
+    private lateinit var purchase: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Initialize the buttons
         this.favorite = findViewById(R.id.star_fab)
+        this.purchase = findViewById(R.id.purchase_btn)
 
         // Set the onClickListener for the buttons
         this.favorite.setOnClickListener {
@@ -28,6 +32,14 @@ class MainActivity : AppCompatActivity() {
                 this.favorite.imageTintList = ContextCompat.getColorStateList(this, R.color.star_default)
             }
 
+        }
+
+        this.purchase.setOnClickListener {
+            // Once pressed, the button will be disabled
+            if (this.purchase.isEnabled)  {
+                this.purchase.isEnabled = false
+                this.purchase.setText(R.string.purchase_text_clicked)
+            }
         }
 
     }
