cf07d9a5dd91dad1b9c6395a305e12ca3e682f2a
erik-lance <eriklance@gmail.com>
2023-09-22 23:51:14 +0800

    Added button functionality for favorite

--- app/src/main/java/com/mobdeve/s13/tiongquico/erik/exercise1/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s13/tiongquico/erik/exercise1/MainActivity.kt
@@ -2,10 +2,33 @@ package com.mobdeve.s13.tiongquico.erik.exercise1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var favorite: FloatingActionButton
+    private var isFavorite: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize the buttons
+        this.favorite = findViewById(R.id.star_fab)
+
+        // Set the onClickListener for the buttons
+        this.favorite.setOnClickListener {
+            this.isFavorite = !this.isFavorite
+
+            // Update the button's icon color from colors.xml
+            if (this.isFavorite) {
+                this.favorite.imageTintList = ContextCompat.getColorStateList(this, R.color.star_pressed)
+            } else {
+                this.favorite.imageTintList = ContextCompat.getColorStateList(this, R.color.star_default)
+            }
+
+        }
+
     }
 }
